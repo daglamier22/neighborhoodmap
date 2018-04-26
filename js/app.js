3c5131544d7de3583181b669526acd37e168558f
Brian McCarthy <daglamier22@gmail.com>
2018-04-26 12:15:34 -0500

    displaying the map centered on my neighborhood

--- js/app.js
+++ js/app.js
@@ -29,11 +29,13 @@ var Location = function(data) {
 var ViewModel = function() {
   var self = this;
 
+  // Create and fill the full locationsList
   this.locationsList = ko.observableArray([]);
   initialLocations.forEach(function(item) {
     self.locationsList.push(new Location(item));
   });
 
+  // These 2 functions are for opening and closing the sidebar when on a smaller screen
   this.w3_open_sidebar = function() {
     document.getElementById("mySidebar").style.display = "block";
   };
@@ -41,9 +43,199 @@ var ViewModel = function() {
     document.getElementById("mySidebar").style.display = "none";
   };
 
+  // handle clicks in sidebar
   this.selectLocation = function() {
     console.log(this.name());
   }
 }
-
 ko.applyBindings(new ViewModel());
+
+///////////////////////////////////////////////////////////////////////
+// Google Maps functionality                                         //
+///////////////////////////////////////////////////////////////////////
+var map;
+
+function errorMap() {
+  alert("Unable to reach Google Maps");
+}
+
+function initMap() {
+  // This style is the WY style from https://snazzymaps.com/style/8097/wy
+  var styles = [
+    {
+      "featureType": "all",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "weight": "2.00"
+        }
+      ]
+    },
+    {
+      "featureType": "all",
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+          "color": "#9c9c9c"
+        }
+      ]
+    },
+    {
+      "featureType": "all",
+      "elementType": "labels.text",
+      "stylers": [
+        {
+          "visibility": "on"
+        }
+      ]
+    },
+    {
+      "featureType": "landscape",
+      "elementType": "all",
+      "stylers": [
+        {
+          "color": "#f2f2f2"
+        }
+      ]
+    },
+    {
+      "featureType": "landscape",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#ffffff"
+        }
+      ]
+    },
+    {
+      "featureType": "landscape.man_made",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#ffffff"
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "all",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "all",
+      "stylers": [
+        {
+          "saturation": -100
+        },
+        {
+          "lightness": 45
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#eeeeee"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#7b7b7b"
+        }
+      ]
+    },
+    {
+      "featureType": "road",
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "color": "#ffffff"
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "all",
+      "stylers": [
+        {
+          "visibility": "simplified"
+        }
+      ]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "transit",
+      "elementType": "all",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "all",
+      "stylers": [
+        {
+          "color": "#46bcec"
+        },
+        {
+          "visibility": "on"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#c8d7d4"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "color": "#070707"
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "color": "#ffffff"
+        }
+      ]
+    }
+  ];
+  // Constructor creates a new map - only center and zoom are required.
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 33.1054826, lng: -96.6803112},
+    zoom: 15,
+    styles: styles,
+    mapTypeControl: false
+  });
+}
