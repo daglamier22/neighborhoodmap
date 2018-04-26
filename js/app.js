0cedf1ffc136819d018b69f4a8e89c7a07a639f3
Brian McCarthy <daglamier22@gmail.com>
2018-04-26 10:46:25 -0500

    created dummy list of locations and displayed them in sidebar

--- js/app.js
+++ js/app.js
@@ -1,5 +1,38 @@
+var initialLocations = [
+  {
+    name: 'Tabby',
+    address: 'img/434164568_fea0ad4013_z.jpg',
+  },
+  {
+    name: 'Tiger',
+    address: 'img/434164568_fea0ad4013_z.jpg',
+  },
+  {
+    name: 'Scaredy',
+    address: 'img/434164568_fea0ad4013_z.jpg',
+  },
+  {
+    name: 'Shadow',
+    address: 'img/434164568_fea0ad4013_z.jpg',
+  },
+  {
+    name: 'Sleepy',
+    address: 'img/434164568_fea0ad4013_z.jpg',
+  },
+];
+
+var Location = function(data) {
+  this.name = ko.observable(data.name);
+  this.address = ko.observable(data.address);
+}
+
 var ViewModel = function() {
   var self = this;
+
+  this.locationsList = ko.observableArray([]);
+  initialLocations.forEach(function(item) {
+    self.locationsList.push(new Location(item));
+  });
 
   this.w3_open_sidebar = function() {
     document.getElementById("mySidebar").style.display = "block";
@@ -7,6 +40,10 @@ var ViewModel = function() {
   this.w3_close_sidebar = function() {
     document.getElementById("mySidebar").style.display = "none";
   };
+
+  this.selectLocation = function() {
+    console.log(this.name());
+  }
 }
 
 ko.applyBindings(new ViewModel());
